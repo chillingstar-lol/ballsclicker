add4cc363d353b654c15c88fc4959ef7a11f1e1c
ChillingStar <72331969+chillingstar@users.noreply.github.com>
2023-08-13 03:23:31 +0700

    Create index.js

---
+++ js/index.js
@@ -0,0 +1,11 @@
+function dl_ballsclicker1_0() {
+    alert("Coming soon...")
+}
+
+function dl_ballsclicker2_0() {
+    alert("Coming soon...")
+}
+
+function dl_ballsclickerx() {
+    alert("Coming soon...")
+}
